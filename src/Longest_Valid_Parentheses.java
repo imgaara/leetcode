ab29193c3c9e80e70d719bbdefeaf955d70dead7
imgaara <imgaara@qq.com>
2014-07-31 01:22:51 +0800

    Sudoku Solver Longest Valid Parentheses

--- src/Longest_Valid_Parentheses.java
+++ src/Longest_Valid_Parentheses.java
@@ -1,9 +1,106 @@
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * Created by Administrator on 2014/7/27 0027.
  */
 public class Longest_Valid_Parentheses {
     public int longestValidParentheses(String s) {
+        if (null == s || s.length() < 2) {
+            return 0;
+        }
 
-        return 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        boolean[] isMatch = new boolean[s.length()+1];
+
+        for (int i = 0; i < s.length(); ++i) {
+            char cur = s.charAt(i);
+            if (cur == '('){
+                stack.push(i);
+            } else {
+                if (stack.isEmpty()) {
+                } else {
+                    int top = stack.pop();
+                    isMatch[i] = true;
+                    isMatch[top] = true;
+                }
+            }
+        }
+
+        int max = 0;
+        int start;
+        for (int i = 0; i < s.length();) {
+            if (isMatch[i]) {
+                start = i;
+                for (int j = i+1; j <= s.length(); ++j) {
+                    if (!isMatch[j]) {
+                        int len = j - start;
+                        if (len > max) {
+                            max = len;
+                        }
+                        i = j + 1;
+                        break;
+                    }
+                }
+            } else {
+                i++;
+            }
+        }
+
+        return max;
+    }
+
+    public int longestValidParentheses2(String s) {
+        final int N = s.length();
+        if (N < 2) {
+            return 0;
+        }
+
+        if (N == 2) {
+            return s.equals("()") ? 2 : 0;
+        }
+
+        if (N == 3) {
+            if (s.equals("(()") || s.equals("())")) {
+                return 2;
+            } else {
+                return 0;
+            }
+        }
+
+        boolean[][] d = new boolean[N][N];
+        for (int i = 0; i < N; ++i) {
+            d[i][i] = false;
+            if (i+1 < N) {
+                d[i][i+1] = (s.charAt(i) == '(' && s.charAt(i+1) == ')');
+            }
+        }
+
+        int max = 0;
+        for (int k = 3; k < N; k += 2) {
+            for (int i = 0; i < N; ++i) {
+                int j = i + k;
+                if (j < N) {
+                    boolean isSub = d[i][i+1] && d[i+2][j];
+                    isSub = isSub || (s.charAt(i) == '(' && s.charAt(j) == ')' && d[i+1][j-1]);
+                    isSub = isSub || (d[j-1][j] && d[i][j-2]);
+
+                    d[i][j] = isSub;
+                    if (isSub) {
+                        if (j - i > max) {
+                            max = j - i;
+                        }
+                    }
+                }
+            }
+        }
+
+        return max + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Longest_Valid_Parentheses().longestValidParentheses(")("));
     }
 }
---
+++ src/Sudoku_Solver.java
@@ -0,0 +1,116 @@
+/**
+ * Created by Administrator on 2014/7/30 0030.
+ */
+public class Sudoku_Solver {
+    public void solveSudoku(char[][] board) {
+        boolean[][] numberInRow = new boolean[9][9];
+        boolean[][] numberInCol = new boolean[9][9];
+        boolean[][] numberInBlock = new boolean[9][9];
+
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                char cur = board[i][j];
+                if ('.' == cur) {
+                    continue;
+                }
+
+                int numberId = cur - '1';
+                int blockId = (i / 3) * 3 + (j / 3);
+
+                numberInRow[i][numberId] = true;
+                numberInCol[j][numberId] = true;
+                numberInBlock[blockId][numberId] = true;
+            }
+        }
+
+        Helper next = nextIJ(0, -1, board);
+        if (null == next) {
+            return;
+        }
+        solveSudoku(board, next.i, next.j, numberInRow, numberInCol, numberInBlock);
+    }
+
+    private boolean solveSudoku(char[][] board, final int i, final int j, boolean[][] numberInRow, boolean[][] numberInCol, boolean[][] numberInBlock) {
+        if (i >= 9) {
+            return true;
+        }
+
+        int blockId = (i / 3) * 3 + (j / 3);
+        for (int k = 0; k < 9; ++k) {
+            if (numberInRow[i][k] || numberInCol[j][k] || numberInBlock[blockId][k]) {
+                continue;
+            }
+
+            numberInRow[i][k] = true;
+            numberInCol[j][k] = true;
+            numberInBlock[blockId][k] = true;
+            board[i][j] = (char)('1' + k);
+
+            Helper next = nextIJ(i, j, board);
+            if (null == next) {
+                return true;
+            }
+
+            if(solveSudoku(board, next.i, next.j, numberInRow, numberInCol, numberInBlock)) {
+                return true;
+            }
+
+            board[i][j] = '.';
+            numberInRow[i][k] = false;
+            numberInCol[j][k] = false;
+            numberInBlock[blockId][k] = false;
+        }
+
+        return false;
+    }
+
+    static class Helper {
+        final int i;
+        final int j;
+        Helper(int i , int j) {
+            this.i = i;
+            this.j = j;
+        }
+    }
+
+    private Helper nextIJ(int i, int j, char[][] board) {
+        int nextI = i;
+        int nextJ = j;
+        while (nextI < 9) {
+            if (nextJ == 8) {
+                nextI = nextI + 1;
+                nextJ = 0;
+            } else {
+                nextJ = nextJ + 1;
+            }
+
+            if (nextI < 9 && board[nextI][nextJ] == '.') {
+                break;
+            }
+        }
+
+        return nextI < 9 ? new Helper(nextI, nextJ) : null;
+    }
+
+    public static void main(String[] args) {
+        char[][] board = new char[][]{
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'},
+        };
+
+        long start = System.currentTimeMillis();
+        new Sudoku_Solver().solveSudoku(board);
+        for (int i = 0; i < board.length; ++i) {
+            System.out.println(board[i]);
+        }
+
+        System.out.println(System.currentTimeMillis() - start);
+    }
+}
